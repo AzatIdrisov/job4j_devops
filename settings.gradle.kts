6cac2a9f08f18c7ac40b42a1fd572dc938a96e12
AzatIdrisov <azzzat72@yandex.ru>
2025-06-27 08:42:59 +0500

    fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -37,17 +37,5 @@ dependencyResolutionManagement {
     }
 }
 
-buildCache {
-    remote<HttpBuildCache> {
-        url = uri(System.getenv("GRADLE_REMOTE_CACHE_URL"))
-        isAllowInsecureProtocol = true
-        isAllowUntrustedServer = true
-        isPush = System.getenv("GRADLE_REMOTE_CACHE_PUSH").toBoolean()
-        credentials {
-            username = System.getenv("GRADLE_REMOTE_CACHE_USERNAME")
-            password = System.getenv("GRADLE_REMOTE_CACHE_PASSWORD")
-        }
-    }
-}
 
 rootProject.name = "DevOps"
